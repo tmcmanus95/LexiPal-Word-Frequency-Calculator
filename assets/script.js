92d28208e77e01b33e3a901be9af7b851610a586
tmcmanus95 <tmcmanus95@gmail.com>
2023-08-30 20:36:14 -0500

    recommiting pseudocode

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,24 @@
-*/ ?*
+/* 
+1. Create elements and variables for those html elements in the javascript    
+    - Title
+    - Input field 
+    - Get frequency button 
+    - Get quote button 
+    - Results container
+    - Word information container
+    - Past searches container 
+    - Language dropdown container (if time)
+
+2. Create function for grabbing the data we want from the word frequency api 
+3. Create function for grabbing the data we want from the quote generator api 
+4. Set the parameters for diffrent word frequency tiers (1000+ very common, 999-500 common, etc.)
+    - Create classes for each tier of word frequency. 
+5. Create a function for determining the frequency of each word inputted, create a span for each word, and assign the class for each word 
+based on word frequecy tier. 
+6. Save searched words to local storage. 
+7. Create a function for searching local storage for past searched words, and rendering those to the page if they exist. 
+8. Create a function for changing the language based on what language is selected in the language drop down menu. 
+9. Create a function so that when each word in the results container is clicked, information about that word and its exact frequency
+of use is displayed in the appropriate container. 
+10. Add stylings to make the user interface not terrible. 
+*/
